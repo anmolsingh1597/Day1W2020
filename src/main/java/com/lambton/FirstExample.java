f4abf60465d03e5f66f60151810be69680feaeb8
Anmol <anmolsingh1597@gmail.com>
2020-01-22 01:49:42 -0500

    Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        System.out.println("Welcome to JAVA programing");
+
+     /*   int j=1;
+        do {
+            System.out.println("Anmol Singh");
+       j++;
+        }
+        while(j<=10);
+*/    }
+}
