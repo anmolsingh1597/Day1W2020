5f9e92eb0574e25ec0eb4643b93e24ecaff26e66
Anmol <anmolsingh1597@gmail.com>
2020-01-22 18:46:52 -0500

    Day 2- Datatypes and Casting

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -11,5 +11,42 @@ public class FirstExample {
        j++;
         }
         while(j<=10);
+
+
+    //Day 2 code starts here;
+        int a=100;
+        int b;
+
+        b=200;
+        int c=a+b;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        System.out.println("Sum = " + (a+b));
+
+        System.out.println(a+b+"Sum");
+
+short x = 32767;
+double f1 =10/3.0;
+double f2 = 10/3;
+int r1 = (int)10.0/3;
+float r2 =10/3;
+float r3 = (float)10/3;
+int x1=1_00_00/10;
+double x2=1_00_00.5_0;
+        System.out.println("MAX Short = "+ Short.MAX_VALUE);
+        System.out.println("MIN Short = "+ Short.MIN_VALUE);
+
+        System.out.println("MAX Long =" + Long.MAX_VALUE);
+
+        System.out.println("F1= "+f1);
+        System.out.println("F2=" +f2);
+        System.out.println("R1= "+r1);
+        System.out.println("R2="+r2);
+        System.out.println("R3="+r3);
+        System.out.println("X1="+x1);
+        System.out.println("X2="+x2);
     }
 }
