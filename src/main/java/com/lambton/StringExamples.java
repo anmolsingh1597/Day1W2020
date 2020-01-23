036c048216a0bd1a4bd7c34a46339ada70ca63c0
Anmol <anmolsingh1597@gmail.com>
2020-01-22 19:15:58 -0500

    Day 2- String Attempt 1

---
+++ src/main/java/com/lambton/StringExamples.java
@@ -0,0 +1,17 @@
+package com.lambton;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        char c='A'+5;
+        String c1="A";
+        int ch=c;
+        System.out.println(c);
+        String y="Hello";
+        String z=new String("Hello ");
+        z=z.concat("World");
+
+        System.out.println("Y="+y);
+
+        System.out.println("Z= "+z);
+    }
+}
